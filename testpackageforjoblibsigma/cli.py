8464fcce350ee533823d8839d5ceef4bee2464bd
tanabe_taketo <tanabe.taketo.nalgo.intern@gmail.com>
2022-12-13 14:21:18 +0900

    add arg comment

--- testpackageforjoblibsigma/cli.py
+++ testpackageforjoblibsigma/cli.py
@@ -24,12 +24,16 @@ def main(args):
 # 引数の処理 （エントリーポイント）
 def entry():
     parser = argparse.ArgumentParser(description="add one float and two integers")
+    # 必須の位置引数
     parser.add_argument("x0", type=float, help="exponent number (float)")
+    # デフォルト値のあるオプションの位置引数 nargsの指定が必須
     parser.add_argument("x1", type=int, nargs="?", default=1, help="start k number (int)")
     parser.add_argument("x2", type=int, nargs="?", default=100, help="start k number (int)")
 
+    # オプション引数 変数名は長い方になる（cは省略形）
     parser.add_argument("-c", "--coefficient", nargs="?", type=float, default=1, help="coefficient number (float)")
 
+    # 排他的で必須なオプション引数
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument("--both", action="store_true", help="exert both for_sigma and joblib_sigma")
     group.add_argument("--for_sigma", action="store_true", help="exert for_sigma")
