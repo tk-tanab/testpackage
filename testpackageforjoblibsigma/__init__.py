645044a9f23d07b0f99084b4453557e4da40002f
tanabe_taketo <tanabe.taketo.nalgo.intern@gmail.com>
2022-12-13 14:28:08 +0900

    upload pypi

--- testpackageforjoblibsigma/__init__.py
+++ testpackageforjoblibsigma/__init__.py
@@ -1,7 +1,10 @@
 # from や import でパッケージ名を指定するとこのファイルが呼ばれる
-# 外部ディレクトリから参照できるようにする関数をimport
+# 外部に公開する関数をimport
 from .for_sigma import for_sigma
 from .joblib_sigma import joblib_sigma
 
-# 関数名を指定
+# 公開する関数名を指定
 __all__ = ["for_sigma", "joblib_sigma"]
+
+# pyproject.tomlと合わせる
+__version__ = "0.1.8"
